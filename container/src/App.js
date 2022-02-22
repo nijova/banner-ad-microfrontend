e6a5c83c565068556ccb7e64f72e21949f52eb9f
nijova <niko.jovan@protonmail.com>
2022-02-22 15:29:08 -0500

    build slot for mf1

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import MicroFrontend from "./MicroFrontend";
 import Home from './Home';
+
+// function Mf1() {
+//   return <MicroFrontend host={mf1Host} name="Mf1" />;
+// }
 
 function App() {
   return (
     <BrowserRouter>
+      {/* <Mf1 /> */}
       <React.Fragment>
         <Routes>
           <Route exact path="/" element={<Home />} />
