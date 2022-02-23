dd2ee564ab1936678b7afa1deda37fc5e34680dd
nijova <niko.jovan@protonmail.com>
2022-02-23 15:58:53 -0500

    add a simple microfrontend

---
+++ mf1/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {;
+  return (
+    <div>
+      <h2>
+        ...very simple MicroFrontend, mf1
+      </h2>
+    </div>
+  );
+}
+
+export default App;
