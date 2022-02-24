385f626675c507ae809839e2e1b79965651966b9
nijova <niko.jovan@protonmail.com>
2022-02-24 15:55:59 -0500

    include mf2 in container app

--- container/src/App.js
+++ container/src/App.js
@@ -4,9 +4,14 @@ import MicroFrontend from "./MicroFrontend";
 import Home from './Home';
 
 const mf1Host = process.env.REACT_APP_MF1_HOST;
+const mf2Host = process.env.REACT_APP_MF2_HOST;
 
 function Mf1() {
   return <MicroFrontend host={mf1Host} name="Mf1" />;
+}
+
+function Mf2() {
+  return <MicroFrontend host={mf2Host} name="Mf2" />;
 }
 
 function App() {
@@ -18,6 +23,7 @@ function App() {
         </Routes>
       </React.Fragment>
       <Mf1 />
+      <Mf2 />
     </BrowserRouter>
   );
 }
